04908cdf684ec52eb2c86e15fc24b809d3fb6808
Denis Anastasev <anastasevdenis@gmail.com>
2026-04-26 15:54:46 +0300

    feat(app): bootstrap default Xcode SwiftUI project

---
+++ golos/golosApp.swift
@@ -0,0 +1,17 @@
+//
+//  golosApp.swift
+//  golos
+//
+//  Created by Alfa on 26.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct golosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ golosTests/golosTests.swift
@@ -0,0 +1,17 @@
+//
+//  golosTests.swift
+//  golosTests
+//
+//  Created by Alfa on 26.04.2026.
+//
+
+import Testing
+@testable import golos
+
+struct golosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
